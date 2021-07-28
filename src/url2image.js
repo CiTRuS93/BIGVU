babc8e78f3538dca340b35ea3ff4711bfe921fe7
CiTRuS93 <hadarradah@gmail.com>
2021-07-28 11:38:40 +0300

    #4 #5 #6

---
+++ src/url2image.js
@@ -0,0 +1,22 @@
+
+const puppeteer = require("puppeteer");
+/**
+ * @param {string} url website url to screenshot
+ * @param {string | URL} filename - path for saving image
+ */
+module.exports = async (url, filename) => {
+    try {
+        new URL(url);
+    } catch (ERR_INVALID_URL) {
+        return false;
+    }
+
+
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+    await page.screenshot({ path: filename });
+    await browser.close();
+    return true;
+
+}
